bbc8aca62024b0a39427d2c24a46002e3d311e5b
Colin Arscott <cma5750@comp-ic-0014.acii.production.int.aci.ics.psu.edu>
2022-02-03 14:53:56 -0500

    Download script finished

--- required/download_data.py
+++ required/download_data.py
@@ -1,4 +1,5 @@
 import requests
+import os
 #This is the base URl to access the API, every request wil start with this
 base = "https://services.cancerimagingarchive.net/nbia-api/services/v1/"
 #The UID is the unique identifier we neeed to access a collection
@@ -14,12 +15,14 @@ json = r.json()
 i = 1
 for item in json:
 	path = "/storage/home/cma5750/DS_440/DS_440_Capstone/volume/data/raw/Images/data"+str(i)+".zip"
-	url = base + item["SeriesInstanceUID"]
+	url = base + "getImage?SeriesInstanceUID=" + item["SeriesInstanceUID"]
 	r = requests.get(url)
 	with open(path, "wb") as fd:
 		for chunk in r.iter_content(chunk_size=128):
 			fd.write(chunk)
 	print(item["SeriesInstanceUID"])
+	exdir = "/storage/home/cma5750/DS_440/DS_440_Capstone/volume/data/raw/Images/"+str(i)+"/"
+	os.system("unzip "+path+ " -d "+ exdir)
 	i = i+1
 
 
